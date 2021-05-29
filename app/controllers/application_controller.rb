8139dfe10919694796ac66a74a4e5b0ddcb805df
valovm <valov.mikhail.a@gmail.com>
2021-05-29 21:57:06 +0300

    Error 404

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,11 +1,17 @@
 class ApplicationController < ActionController::API
 
   rescue_from(StandardError) do |e|
-    p e
     render json: {
         errors: [{code: 'error', title: I18n.t('errors.common.title'), detail: I18n.t('errors.common.detail')}],
     }, status: 500
   end
+
+  rescue_from(ActiveRecord::RecordNotFound) do |e|
+    render json: {
+        errors: [{code: 'error', title: I18n.t('errors.not_found.title'), detail: I18n.t('errors.not_found.detail')}],
+    }, status: 404
+  end
+
 
   rescue_from(BaseError) do |e|
     render json: {errors: [e]}, status: :bad_request
